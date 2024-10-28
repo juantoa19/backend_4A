208a074b12ca107a1a733004ed5974f4ce73834f
Juantoaso18 <erick.toaso14@gmail.com>
2024-10-28 18:25:05 -0500

    Login auth jwt(1)

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,8 +1,9 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+@UseGuards(JwtAuthGuard)
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
@@ -12,6 +13,7 @@ export class UsersController {
     return this.usersService.create(createUserDto);
   }
 
+  
   @Get()
   findAll() {
     return this.usersService.findAll();
